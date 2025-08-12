e22c7a5dcb4befc436231b2d1395f1faa2c450ac
kroozin <hourigan.brian@gmail.com>
2025-08-12 14:32:23 -0500

    Working, but text indenting

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from stats import word_count
 from stats import char_count
+from stats import sort_on
+from stats import convert_to_list
 
 def get_book_text(file):
     with open(file) as f:
         return f.read()
+    
+def generate_report(dict):
+    report = ""
+    for d in dict:
+        if d["char"].isalpha():
+            report += d["char"] + ": " + str(d["num"]) + "\n"
+    return report
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = word_count(text)
-    print(f"{num_words} words found in the document")
-    print(char_count(text))
-
-
+    report_output = '''============ BOOKBOT ============
+    Analyzing book found at books/frankenstein.txt...
+    ----------- Word Count ----------\nFound ''' + str(num_words) + " total words\n--------- Character Count -------\n"
+    full_dict = char_count(text)
+    dict_list = convert_to_list(full_dict)
+    dict_list.sort(reverse = True, key = sort_on)
+    report_output += generate_report(dict_list)
+    print(report_output)
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,19 @@ def char_count(text):
         else:
             letter_count[t] = 1
     return letter_count
+
+#Take a dictionary and break it into a list of dictionaries
+def convert_to_list(items):
+    dict_list = []
+    for item in items:
+        num = items[item]
+        #Iterate through the dictionary and make each char/num its own value/pair dictionary
+        char_dict = {
+            "char": item,
+            "num": num
+        }
+        dict_list.append(char_dict)
+    return dict_list
+
+def sort_on(items):
+    return items["num"]
