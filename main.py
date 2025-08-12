c715cec3a54ece23650fa4e36bcc0e5daaa3cb43
kroozin <hourigan.brian@gmail.com>
2025-08-12 14:45:13 -0500

    Refactor to fix text output

--- main.py
+++ main.py
@@ -14,16 +14,22 @@ def generate_report(dict):
             report += d["char"] + ": " + str(d["num"]) + "\n"
     return report
 
+def print_report(num, data):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num} total words")
+    print("--------- Character Count -------")
+    print(data)
+    print("============= END ===============")
+
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = word_count(text)
-    report_output = '''============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
-    ----------- Word Count ----------\nFound ''' + str(num_words) + " total words\n--------- Character Count -------\n"
     full_dict = char_count(text)
     dict_list = convert_to_list(full_dict)
     dict_list.sort(reverse = True, key = sort_on)
-    report_output += generate_report(dict_list)
-    print(report_output)
+    report_data = generate_report(dict_list)
+    print_report(num_words, report_data)
 
 main()
